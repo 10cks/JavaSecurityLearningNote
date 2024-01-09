b289a042d8be82c290574afff3361cedd965520d
10cks <47177550+10cks@users.noreply.github.com>
2024-01-09 20:26:08 +0800

    Add files via upload

---
+++ springboot-03-web/src/main/java/com/example/springboot03web/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot03web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello,world";
+    }
+}
---
+++ springboot-03-web/src/test/java/com/example/springboot03web/Springboot03WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot03web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
